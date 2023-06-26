cbb98000b9594bbb35f5b204145b572e47582106
seojuntaek <jwutn908@gmail.com>
2023-06-26 17:55:55 +0900

    프로필 수정하기 기능 디비연동

--- client/src/components/ProfileDialog.tsx
+++ client/src/components/ProfileDialog.tsx
@@ -17,9 +17,10 @@ import TextField from '@mui/material/TextField';
 import { setShowProfile } from '../stores/UserStore'
 import { useAppSelector, useAppDispatch } from '../hooks'
 import { getMyProfile } from '../apicalls/auth';
+import { UpdateRequest, updateMyProfile } from '../apicalls/auth';
 
 import Button from '@mui/material/Button'
-
+import axios from 'axios'
 
 const Backdrop = styled.div`
   position: fixed;
@@ -34,6 +35,7 @@ const Wrapper = styled.div`
   height: 100%;
   margin-top: auto;
 `
+const Form = styled.form``
 
 const Content = styled.div`
   margin: 70px auto;
@@ -104,9 +106,13 @@ export default function ProfileDialog() {
 
   const dispatch = useAppDispatch()
 
-  const [git, setGit] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
+  const [grade, setGrade] = useState<string>('') // git
+  const [school, setSchool] = useState<string>('') // email
   const [message, setMessage] = useState<string>('')
+
+  const [gradeFieldEmpty, setGradeFieldEmpty] = useState<boolean>(false)
+  const [schoolFieldWrongFormat, setSchoolFieldEmpty] = useState<boolean>(false)
+  const [messageFieldEmpty, setmessageFieldEmpty] = useState<boolean>(false)
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -128,8 +134,8 @@ export default function ProfileDialog() {
         .then((response) => {
           if (!response) return;
           const { contactGit, contactEmail, profileMessage } = response
-          setGit(contactGit)
-          setEmail(contactEmail)
+          setGrade(contactGit)
+          setSchool(contactEmail)
           setMessage(profileMessage)
         })
         .catch((error) => {
@@ -144,71 +150,132 @@ export default function ProfileDialog() {
     setOpen(!open);
   };
 
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // reset the error message
+    setGradeFieldEmpty(false)
+    setSchoolFieldEmpty(false)
+    setmessageFieldEmpty(false)
+
+    event.preventDefault()
+
+    if (grade === '') {
+      setGradeFieldEmpty(true)
+    } else if (school === '') {
+      setSchoolFieldEmpty(true)
+    } else if (message === '') {
+      setmessageFieldEmpty(true)
+    } else {
+      const body: UpdateRequest = {
+        username: username,
+        character: character,
+        contactGit: grade,
+        contactEmail: school,
+        profileMessage: message,
+      }
+      updateMyProfile(body)
+        .then((response) => {
+          if (response.status === 200) {
+            const { contactGit, contactEmail, profileMessage } = response.payload
+            setGrade(contactGit)
+            setSchool(contactEmail)
+            setMessage(profileMessage)
+            console.log('성공')
+            setIsOpen(false)
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            const { status, message } = error.response.data
+            console.log(message)
+          }
+        })
+    }
+  }
+
   return (
     <Backdrop>
-        <Wrapper>
-          <Content>
-            <ChatHeader>
-              <Title>
-                내소개
-              </Title>
-              <IconButton
-                aria-label="close dialog"
-                className="close"
-                onClick={() => dispatch(setShowProfile(false))}
-                size="small"
-              >
-                <CloseIcon />
-              </IconButton>
-            </ChatHeader>
-            <ChatBox>
-              <List>
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar src={imgpath} />
-                  </ListItemAvatar>
+      <Wrapper>
+        <Content>
+          <ChatHeader>
+            <Title>내소개</Title>
+            <IconButton
+              aria-label="close dialog"
+              className="close"
+              onClick={() => dispatch(setShowProfile(false))}
+              size="small"
+            >
+              <CloseIcon />
+            </IconButton>
+          </ChatHeader>
+          <ChatBox>
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar src={imgpath} />
+                </ListItemAvatar>
+                <Profile>
+                  레벨 {userLevel}
+                  <br />
+                  <br />
+                  <strong>{username}</strong>님
+                </Profile>
+              </ListItem>
+            </List>
+            {isOpen ? (
+              <>
+                <Form onSubmit={handleSubmit}>
+                  <TextDiv>
+                    <TextField
+                      label="학년"
+                      id="standard-size-normal"
+                      defaultValue=""
+                      onChange={(e) => setGrade(e.target.value)}
+                      variant="standard"
+                    />
+                    <TextField
+                      label="학교"
+                      id="standard-size-normal"
+                      defaultValue=""
+                      onChange={(e) => setSchool(e.target.value)}
+                      variant="standard"
+                    />
+                    <TextField
+                      id="standard-size-normal"
+                      label="자기소개"
+                      defaultValue=""
+                      onChange={(e) => setMessage(e.target.value)}
+                      variant="standard"
+                    />
+                  </TextDiv>
+                  <Buttons>
+                    <Button variant="contained" size="large" type="submit">
+                      수정완료
+                    </Button>
+                  </Buttons>
+                </Form>
+              </>
+            ) : (
+              <>
+                <TextDiv>
                   <Profile>
-                    레벨 {userLevel}<br/><br/>
-                    <strong>{username}</strong>님
+                    학년: {grade} <br></br>
+                    <br />
+                    학교: {school} <br></br>
+                    <br />
+                    자기소개: {message} <br></br>
+                    <br />
                   </Profile>
-                </ListItem>
-              </List>
-
-              <TextDiv>
-              <TextField
-                label="학교"
-                id="standard-size-normal"
-                defaultValue=""
-                variant="standard"
-              />
-              </TextDiv>
-
-              <TextDiv>
-              <TextField
-                label="학년"
-                id="standard-size-normal"
-                defaultValue=""
-                variant="standard"
-              />
-              </TextDiv>
-
-              <TextDiv>
-              <TextField
-                id="standard-size-normal"
-                label="자기소개"
-                defaultValue=""
-                variant="standard"
-              />
-              </TextDiv>
-
-              <Buttons>
-                <Button onClick={() => console.log("click") }>
-                  수정하기
-                </Button>
-              </Buttons>
-            </ChatBox>
-          </Content>
-        </Wrapper>
+                </TextDiv>
+                <Buttons>
+                  <Button onClick={() => setIsOpen(true)}>수정하기</Button>
+                </Buttons>
+              </>
+            )}
+          </ChatBox>
+        </Content>
+      </Wrapper>
     </Backdrop>
   )
 }
--- server/controllers/UserControllers/index.ts
+++ server/controllers/UserControllers/index.ts
@@ -244,13 +244,14 @@ export const updateProfile = async (req: CustomRequest, res: Response) => {
         message: '유저 데이터 조회 실패'
     })
 
-    const existingUsername = await User.findOne({ username: newUserData.username })
-    if (existingUsername) {
-        return res.status(410).json({
-            status: 410,
-            message: '이미 사용중인 닉네임입니다.'
-        })
-    }
+    // 프로필 수정시에는 username을 변경하지 않기 때문에 오류가 발생하여 주석처리 했습니다.
+    // const existingUsername = await User.findOne({ username: newUserData.username })
+    // if (existingUsername) {
+    //     return res.status(410).json({
+    //         status: 410,
+    //         message: '이미 사용중인 닉네임입니다.'
+    //     })
+    // }
 
     foundUser.username = newUserData.username
     foundUser.userProfile!.character = newUserData.character
