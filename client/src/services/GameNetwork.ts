41b608bbed19c623009028649a3caea26edb90c8
chohk10 <chohk201@gmail.com>
2023-06-28 17:52:33 +0900

    fix: Make game lobby be joined multiple times

--- client/src/services/GameNetwork.ts
+++ client/src/services/GameNetwork.ts
@@ -1,19 +1,23 @@
 import { Client, Room } from 'colyseus.js'
-// import { IOfficeState, IPlayer, IMoleGame, IBrickGame, IRainGame, IFaceChat } from '../../../types/IOfficeState'
-import { IGameState, IGamePlayer, IBrickPlayer, IBrickGameState } from '../../../types/IGameState'
+import { IGameRoomData, RoomType } from '../../../types/Rooms'
+import { IGameState } from '../../../types/IGameState'
 import { Message } from '../../../types/Messages'
-import { IGameRoomData, IRoomData, RoomType } from '../../../types/Rooms'
-import { ItemType } from '../../../types/Items'
-import { phaserEvents, Event } from '../events/EventCenter'
-import WebRTC from '../web/WebRTC'
 import store from '../stores'
-import { setPlayerNameMap, removePlayerNameMap, setGameSessionId } from '../stores/UserStore'
+import { setGameSessionId } from '../stores/UserStore'
 import { 
   setMoleGameFriendInfo,
   setMoleGameFriendData
  } from '../stores/MoleGameStore'
+import { 
+  setBrickGameState,
+  setMyPlayerScore,
+  setMyPlayerStatus, 
+  setOppPlayerScore, 
+  setOppPlayerStatus 
+} from '../stores/BrickGameStore'
 import {
   setLobbyJoined,
+  setGameJoined,
   setJoinedGameRoomData,
   setGamePlayers,
   setAvailableBrickRooms,
@@ -22,8 +26,8 @@ import {
   setAvailableFaceChatRooms,
   addAvailableRooms,
   removeAvailableRooms,
+  clearAvailabelGameRooms,
 } from '../stores/RoomStore'
-import { setBrickGameState, setMyPlayerScore, setMyPlayerStatus, setOppPlayerScore, setOppPlayerStatus } from '../stores/BrickGameStore'
 
 export default class GameNetwork {
   private client: Client
@@ -38,10 +42,6 @@ export default class GameNetwork {
         ? import.meta.env.VITE_SERVER_URL
         : `${protocol}//${window.location.hostname}:2567`
     this.client = new Client(endpoint)
-    
-    // phaserEvents.on(Event.MY_PLAYER_NAME_CHANGE, this.updatePlayerName, this)
-    // phaserEvents.on(Event.MY_PLAYER_TEXTURE_CHANGE, this.updatePlayer, this)
-    // phaserEvents.on(Event.PLAYER_DISCONNECTED, this.playerStreamDisconnect, this)
   }
 
   async leaveGameRoom() {
@@ -50,19 +50,20 @@ export default class GameNetwork {
       this.room = undefined
       this.mySessionId = ''
       store.dispatch(setGameSessionId(''))
+      store.dispatch(setGameJoined(false))
+      store.dispatch(setJoinedGameRoomData({ id: '', name: '', description: '' }))
     }
   }
 
-  async leaveLobbyRoom() {
+  leaveLobbyRoom() {
     if (this.lobby) {
-      await this.lobby.leave()
+      this.lobby.leave()
+      store.dispatch(clearAvailabelGameRooms())
     }
     this.lobby = undefined
   }
   
   async joinLobbyRoom(type: RoomType) {
-    if (this.lobby) return
-
     this.lobby = await this.client.joinOrCreate(type)
     store.dispatch(setLobbyJoined(true))
 
@@ -154,36 +155,6 @@ export default class GameNetwork {
     store.dispatch(setLobbyJoined(false))
     this.mySessionId = this.room.sessionId
     store.dispatch(setGameSessionId(this.room.sessionId)) 
-    // // this.webRTC = new WebRTC(this.mySessionId, this)
-
-    // // new instance added to the players MapSchema
-    // this.room.state.players.onAdd = (player: IPlayer, key: string) => {
-    //   if (key === this.mySessionId) return
-
-    //   // track changes on every child object inside the players MapSchema
-    //   player.onChange = (changes) => {
-    //     changes.forEach((change) => {
-    //       const { field, value } = change
-    //       // phaserEvents.emit(Event.PLAYER_UPDATED, field, value, key)
-
-    //       // when a new player finished setting up player name
-    //       if (field === 'name' && value !== '') {
-    //         // phaserEvents.emit(Event.PLAYER_JOINED, player, key)
-    //         store.dispatch(setPlayerNameMap({ id: key, name: value }))
-    //         store.dispatch(pushPlayerJoinedMessage(value))
-    //       }
-    //     })
-    //   }
-    // }
-
-    // // an instance removed from the players MapSchema
-    // this.room.state.players.onRemove = (player: IPlayer, key: string) => {
-    //   // phaserEvents.emit(Event.PLAYER_LEFT, key)
-    //   // this.webRTC?.deleteVideoStream(key)
-    //   // this.webRTC?.deleteOnCalledVideoStream(key)
-    //   store.dispatch(pushPlayerLeftMessage(player.name))
-    //   store.dispatch(removePlayerNameMap(key))
-    // }
 
     // when the server sends room data
     this.room.onMessage(Message.SEND_ROOM_DATA, (content) => {
@@ -217,11 +188,11 @@ export default class GameNetwork {
     this.room?.send(Message.UPDATE_PLAYER_NAME, { name: currentName })
   }
 
-  // method to send ready-to-connect signal to Colyseus server
-  readyToConnect() {
-    this.room?.send(Message.READY_TO_CONNECT)
-    phaserEvents.emit(Event.MY_PLAYER_READY)
-  }
+  // // method to send ready-to-connect signal to Colyseus server
+  // readyToConnect() {
+  //   this.room?.send(Message.READY_TO_CONNECT)
+  //   phaserEvents.emit(Event.MY_PLAYER_READY)
+  // }
 
   brick_game_init() {
     if (!this.room) return
