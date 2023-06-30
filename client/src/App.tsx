be229a7968cb3ab77a92f957e02e4c804c076d98
Jiwon <moonlight_youlike@naver.com>
2023-06-30 19:38:41 +0900

    Brick Game: move the png files to public directory.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,7 @@ import { authenticateUser } from './apicalls/auth';
 // const cookies = new Cookies();
 
 // TODO: Production 서버에 옮겨가면 해당 부분 수정 필요 
-axios.defaults.baseURL = 'http://43.200.172.83:2567'
+axios.defaults.baseURL = 'http://localhost:2567'
 console.log('axios.defaults.baseURL ', axios.defaults.baseURL);
 
 const Backdrop = styled.div`
