e8d44e86b21318e400f109c32fb72722cd2a48f0
chohk10 <chohk201@gmail.com>
2023-07-03 15:51:25 +0900

    fix: Users & Friends Dialog resolve merge conflict

--- client/src/components/helperdialog/FriendsDialog.tsx
+++ client/src/components/helperdialog/FriendsDialog.tsx
@@ -6,12 +6,13 @@ import CloseIcon from '@mui/icons-material/Close'
 import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Avatar from '@mui/material/Avatar'
-import { PersonOffOutlined, PersonRemoveOutlined, MailOutlineRounded } from '@mui/icons-material'
+import { PersonRemoveOutlined, MailOutlineRounded } from '@mui/icons-material'
 import { Content, Header, HeaderTitle } from '../GlobalStyle'
 
 import { setShowDMList, setShowDMRoom } from '../../stores/DMStore'
 import { HELPER_STATUS, setHelperStatus } from '../../stores/UserStore'
 import { useAppSelector, useAppDispatch } from '../../hooks'
+import { getUserProfile } from '../../apicalls/auth'
 import {
   getFriendList,
   getFriendRequestList,
@@ -91,6 +92,16 @@ const ProfileButton = styled.div`
     color: black;
   }
 `
+const TextDiv = styled.div`
+  margin-bottom: 10px;
+`
+const Profile = styled.div`
+  color: white;
+  width: 200px;
+  font-size: 18px;
+  font-family: Font_DungGeun;
+  text-align: left;
+`
 
 export default function FriendsDialog() {
   const dispatch = useAppDispatch()
@@ -102,6 +113,13 @@ export default function FriendsDialog() {
   const helperStatus = useAppSelector((state) => state.user.helperStatus)
   const [friendList, setFriendList] = useState<friendListInterface[]>([])
   const [friendRequestList, setFriendRequestList] = useState<friendListInterface[]>([])
+  const [friendUsername, setFriendUsername] = useState<string>('')
+  const [friendCharacter, setFriendCharacter] = useState<string>('')
+  const [friendUserLevel, setFriendUserLevel] = useState<string>('')
+  const [grade, setGrade] = useState<string>('')
+  const [school, setSchool] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -202,8 +220,87 @@ export default function FriendsDialog() {
       })
   }
 
+  const getFriendProfile = (username: string) => {
+    getUserProfile(username)
+      .then((response) => {
+        if (!response) return
+        const { username, character, userLevel, grade, school, profileMessage } = response
+        setFriendUsername(username)
+        setFriendCharacter(character)
+        setFriendUserLevel(userLevel)
+        setGrade(grade)
+        setSchool(school)
+        setMessage(profileMessage)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  const openModal = () => {
+    setTimeout(() => {
+      setIsModalOpen(true)
+    }, 200)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const Modal = ({ open, handleClose }: { open: any; handleClose: any }) => {
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: '#222639',
+          borderRadius: '24px',
+          boxShadow: '0px, 10px, 24px, #0000006f',
+          padding: '50px',
+          zIndex: 1000,
+          fontSize: '15px',
+          color: '#eee',
+          textAlign: 'center',
+          fontFamily: 'Font_DungGeun',
+        }}
+      >
+        <div>
+          <div>
+            <img
+              src={`../../public/assets/character/single/${friendCharacter}_idle_anim_19.png`}
+              alt=""
+            />
+            <div>
+              <h2>Lv: {friendUserLevel}</h2>
+              <h2>{friendUsername}</h2>
+            </div>
+          </div>
+          <>
+            <TextDiv>
+              <Profile>
+                학년: {grade} <br /><br />
+                학교: {school} <br /><br />
+                자기소개: {message} <br /><br />
+              </Profile>
+            </TextDiv>
+          </>
+        </div>
+        <Button
+          variant="contained"
+          onClick={handleClose}
+          style={{ fontWeight: 'bold', margin: 'auto' }}
+        >
+          확인
+        </Button>
+      </div>
+    )
+  }
+
   return (
     <Backdrop>
+      {isModalOpen && <Modal open={isModalOpen} handleClose={closeModal} />}
       <Wrapper>
         <Content>
           <Header>
@@ -223,6 +320,10 @@ export default function FriendsDialog() {
             )}
             {friendRequestList.map((value, index) => (
               <ListItem divider key={index} style={{ padding: '16px'}}>
+                <Button onClick={() => {
+                  getFriendProfile(value.username)
+                  openModal()
+                }}>
                 <NameWrapper>
                   <ListItemAvatar>
                     <Avatar
@@ -234,6 +335,7 @@ export default function FriendsDialog() {
                     <Username>{value.username}</Username>
                   </NameProfile>
                 </NameWrapper>
+                </Button>
                 <ProfileButton>
                   <Button
                     onClick={() => receiveReject(value.username, username)}
@@ -255,6 +357,10 @@ export default function FriendsDialog() {
             )}
             {friendList.map((value, index) => (
               <ListItem divider key={index} style={{ padding: '16px'}}>
+                <Button onClick={() => {
+                  getFriendProfile(value.username)
+                  openModal()
+                }}>
                 <NameWrapper>
                   <ListItemAvatar>
                     <Avatar
@@ -266,6 +372,7 @@ export default function FriendsDialog() {
                     <Username>{value.username}</Username>
                   </NameProfile>
                 </NameWrapper>
+                </Button>
                 <ProfileButton>
                   <Button onClick={() => removeFriendList(value.username, username)}>
                     <PersonRemoveOutlined fontSize='large' style={{ color: 'gray'}} />
