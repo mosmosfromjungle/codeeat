9ed270fdf8e57230847009ecbcce709e2dbc58c0
whwogur <cjh.lux@gmail.com>
2023-07-04 19:44:00 +0900

    dm list css

--- client/src/components/DM/DMList.tsx
+++ client/src/components/DM/DMList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
+import { Content, Header, HeaderTitle } from '../GlobalStyle'
 import {
   setNewMessage,
   setReceiverName,
@@ -14,9 +15,6 @@ import {
   fetchRoomList,
   RoomListResponse,
 } from '../../apicalls/DM/DM';
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
-import DefaultAvatar from '../../images/login/Lucy_login.png'
 
 /* DM목록을 불러온다.  */
 export const ConversationList = () => {
@@ -24,7 +22,6 @@ export const ConversationList = () => {
   const dispatch = useAppDispatch();
   const username = useAppSelector((state) => state.user.username);
   useEffect(() => {
-    console.log('방가져온다',username)
     fetchRoomList(username)
     .then((data) => {
         setRooms(data);
@@ -36,16 +33,16 @@ export const ConversationList = () => {
   }, [rooms]);
 
 const handleClick = (room) => {
-  dispatch(setReceiverName(room.receiverName == username ? room.senderName : room.receiverName));
+  dispatch(setReceiverName(room.receiverName));
   dispatch(setRoomId(room.roomId));
   console.log('룸아이디설정',room.roomId)
   dispatch(setShowDMRoom(true))
 }
 return (
     <Backdrop>
-        <DMwrapper>
-        <DMHeader>
-              <Title> DM 목록 </Title>
+        <Content>
+        <Header>
+              <HeaderTitle> DM 목록 </HeaderTitle>
               <IconButton
                 aria-label="close dialog"
                 className="close"
@@ -54,54 +51,54 @@ return (
               >
                 <CloseIcon />
               </IconButton>
-            </DMHeader>
+            </Header>
         <DMList>
         {rooms.length !== 0 ? (
           rooms.map((room) => {
             return (
-            <ListTag
+              <ListTag
                 key={room._id}
                 onClick={() => {
                   dispatch(setShowDMList(false))
                   handleClick(room);
-                }}
-              >
+                }}>
                 <UserNamewithLastMessage>
-                  <UserName>{room.senderName}</UserName>
+                  <UserName>{room.receiverName}</UserName>
                     <LastMessage>
                       {room.message}
                     </LastMessage>
                 </UserNamewithLastMessage>
               </ListTag>
-              )
+            );
           })
         ) : (
           <>
-            <NoDMMessage> <strong>📭 아직 대화방이 없어요</strong><br></br><br></br><br></br> </NoDMMessage>
+            <NoDMMessage> <strong> 📭 아직 대화방이 없어요</strong><br></br><br></br><br></br> </NoDMMessage>
             <NoDMMessage> <strong>다른 플레이어와 개인대화를 시작해보세요!</strong><br></br> </NoDMMessage>
           </>
         )}
         </DMList>
-      </DMwrapper>
+      </Content>
     </Backdrop>
   );
 };
 
 const ListTag = styled.li`
-  width: 335px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  cursor: pointer;
-  padding: 5px;
-  margin-bottom: 10px;
+width: 100%;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: flex-start;
+cursor: pointer;
+padding: 16px;
+margin-bottom: 10px;
+border-bottom: 2px solid black;
 `;
 const UserNamewithLastMessage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  color: white;
+  color: black;
   justify-content: space-between;
   padding: 0px 0px 0px 25px;
   border-bottom: 1px solid gray;
@@ -109,17 +106,20 @@ const UserNamewithLastMessage = styled.div`
   font-size: 10px;
   height: 60px;
   font-family: Font_DungGeun;
-`;
+
+  &:last-child {
+    border-bottom: none;
+  }`;
 const UserName = styled.div`
   display: block;
-  font-size: 15px;
-  margin: 0px 0px 10px 0px;
+  font-size: 25px;
+  margin: 0px 0px 10px -10px;
   font-weight: bold;
-  color: #33FF99;
+  color: green;
 `;
 const LastMessage = styled.div`
   display: block;
-  font-size: 1.5em;
+  font-size: 20px;
   margin: 0px 0px 10px 0px;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -131,53 +131,28 @@ const Backdrop = styled.div`
 position: fixed;
 display: flex;
 gap: 10px;
-bottom: 75px;
+bottom: 16px;
 right: 16px;
 align-items: flex-end;
 `
 const DMList = styled.div`
-height: 500px;
-width: 360px;
-overflow: auto;
-background: #2c2c2c;
-border: 1px solid #00000029;
-padding: 10px 10px;
-border-radius: 0px 0px 10px 10px;
-
-Button {
-  font-size: 17px;
-  font-family: Font_DungGeun;
-}
+flex: 1;
+  height: calc(100% - 76px);
+  overflow: auto;
+  padding: 10px 0 0 20px;
+  display: flex;
+  flex-direction: column;
+  
+  .listitem && {
+    flex: 0 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 const NoDMMessage = styled.div`
-  color: white;
+  color: black;
   width: 300px;
   font-size: 20px;
   font-family: Font_DungGeun;
-`
-const DMHeader = styled.div`
-  position: relative;
-  height: 40px;
-  background: #000000a7;
-  border-radius: 10px 10px 0px 0px;
-
-  .close {
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-`
-const Title = styled.div`
-  position: absolute;
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
-  top: 10px;
-  left: 150px;
-  font-family: Font_DungGeun;
-`
-const DMwrapper = styled.div`
-  height: 100%;
-  margin-top: auto;
 `
